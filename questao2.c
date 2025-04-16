3e0bd2df720a3317fbd247d07c210b9410c3e530
Sophia Garcia <sophiagarccia2@gmail.com>
2025-04-16 11:29:16 -0300

    Adicionando o esqueleto inicial da questao 2

---
+++ questao2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    
+    char frase[500];
+    char *tokens[500];
+    char nome[500], numero[500], ponto[500];
+    int i = 0;
+    
+    printf("Digite sua lista aqui:\n");
+    while(fgets(frase, sizeof(frase), stdin)) {
+    
+        char *token = strtok(frase, " ");
+        
+        while(token != NULL) {
+            token[i];
+            i++;
+            token = strtok(NULL, ",");
+        }
+        
+        for(i = 0; i < strlen(token); i++){
+            
+            // Ler nome
+            if(isalpha(token[i]) ){
+                
+                /* // Evita acessar além do tamanho do nome
+                if (i < strlen(nome)) { */
+                
+                token[i] = nome[i];
+            }
+            // Ler numero
+            // precisa diferencia aqui se é int ou float
+            if(isdigit(token[i])){
+                token[i] = numero[i];
+            }
+            if(strncmp(token[i], "(", 1) == 0){
+                token[i] = ponto[i];
+            }
+        }
+        
+    }
+    
+    
+    
+}
